f325f0f1f223443f0e3a40365ea88e8f05896329
anilc55 <chauhananilkumar906@gmail.com>
2026-02-01 09:32:24 +0530

    Create Navbar.js

---
+++ app/app/app/dashboard/app/components/Navbar.js
@@ -0,0 +1,35 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <div style={navStyle}>
+      <h2>Silent Finder</h2>
+
+      <div>
+        <Link href="/" style={linkStyle}>
+          Home
+        </Link>
+
+        <Link href="/dashboard" style={linkStyle}>
+          Dashboard
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+const navStyle = {
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  padding: "15px 30px",
+  background: "#000",
+  color: "#fff"
+};
+
+const linkStyle = {
+  marginLeft: "20px",
+  color: "#fff",
+  textDecoration: "none",
+  fontWeight: "bold"
+};
